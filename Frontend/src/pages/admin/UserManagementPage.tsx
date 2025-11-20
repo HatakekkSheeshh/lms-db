0608f4139b4d374e23de94c0dc9ff29c350f5df5
Nguyen Quoc Hieu <nguyenquochieujff7@gmail.com>
2025-11-20 22:04:46 +0700

    fix: firstname -> fullname

--- Frontend/src/pages/admin/UserManagementPage.tsx
+++ Frontend/src/pages/admin/UserManagementPage.tsx
@@ -438,7 +438,7 @@ export default function UserManagementPage() {
             onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
             className="h-8 px-2 lg:px-3"
           >
-            {t('admin.firstName')}
+            {t('admin.fullName')}
             <ArrowUpDown className="ml-2 h-4 w-4" />
           </Button>
         )
